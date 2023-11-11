cf90c104d485f0173909dc4fdc840b796a391442
Younghoon Ahn <ahnqwerty@gmail.com>
2023-11-11 18:13:45 +0900

    refactor: 리팩토링

---
+++ ZombieRunner/DrawStage/drawStage.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<windows.h>
+
+int drawStage() {
+
+
+
+	return TRUE;
+}
+
+int showMap() {
+
+
+	return TRUE;
+}
+
+int showPC(char pcInfo[3][5]) {
+
+
+	return TRUE;
+}
+
+int removePC(char pcInfo[3][5]) {
+
+
+	return TRUE;
+}
+
+int showZombie(char zombieInfo[3][5]) {
+
+
+	return TRUE;
+}
+
+int removeZombie(char zombieInfo[3][5]) {
+
+
+	return TRUE;
+}
