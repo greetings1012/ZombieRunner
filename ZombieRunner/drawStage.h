ef23364d427f8abdd513ae64895cb370ed153d01
Younghoon Ahn <41965535+greetings1012@users.noreply.github.com>
2023-11-11 17:50:51 +0900

    Add files via upload

---
+++ ZombieRunner/drawStage.h
@@ -0,0 +1,13 @@
+#pragma once
+
+int drawStage();
+
+int showMap();
+
+int showPC(char pcInfo[3][5]);
+
+int removePC(char pcInfo[3][5]);
+
+int showZombie(char zombieInfo[3][5]);
+
+int removeZombie(char zombieInfo[3][5]);
