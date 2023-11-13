798efc771a61be7d534e020db3f1c8b08ed4d419
sungminstar <sungmin8453@naver.com>
2023-11-13 20:44:44 +0900

    movePC ver 1.0

---
+++ ZombieRunner/movePC/pcHighJump.c
@@ -0,0 +1,7 @@
+// ← : 높은 점프
+#include "pcMovement.h"
+
+int pcHighJump(void) {
+	printf("HighJump\n");
+	return HIGH_JUMP;
+}
---
+++ ZombieRunner/movePC/pcMovement.h
@@ -0,0 +1,14 @@
+#ifndef PC_MOVEMENT_H
+#define PC_MOVEMENT_H
+
+#define ATTACK 77
+#define HIGH_JUMP 75
+#define JUMP 72
+#define SLIDE 80
+
+int pcAttack(void);
+int pcHighJump(void);
+int pcJump(void);
+int pcSlide(void);
+
+#endif
