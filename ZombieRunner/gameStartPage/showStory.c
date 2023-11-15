f9e0b6a8c71ea44e40ba77c59684fbdf33f44766
sungminstar <sungmin8453@naver.com>
2023-11-15 14:29:46 +0900

    gameStartPage -ver 0.1

---
+++ ZombieRunner/gameStartPage/showStory.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <conio.h>
+#include "page.h"
+
+void showStory(void) {
+    ShowPage(Story, 7);
+}
---
+++ ZombieRunner/gameStartPage/showTitle.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <conio.h>
+#include "page.h"
+
+void showTitle(void) {
+    ShowPage(Title, 1);
+}
